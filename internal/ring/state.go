ca3a09689d47bbd26128a95159b6751298f1db9b
jiayun <jiayun_work@163.com>
2026-08-19 02:36:11 +0800

    chore: single independent bug (JYY-0143)

---
+++ internal/ring/state.go
@@ -0,0 +1,19 @@
+package ring
+
+func (r *Ring) replaceKeys(next []uint32) {
+	r.keys = append(r.keys, next...)
+}
+
+// Members returns all physical node names currently in the ring.
+func (r *Ring) Members() []string {
+	names := make([]string, 0, len(r.members))
+	for n := range r.members {
+		names = append(names, n)
+	}
+	return names
+}
+
+// Replicas returns the configured number of virtual nodes per physical node.
+func (r *Ring) Replicas() int {
+	return r.replicas
+}
